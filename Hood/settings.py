c7ca3be45c4bea0feeee9931082200e5732d67f2
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-05-31 11:03:15 +0300

    Adding registration files

--- Hood/settings.py
+++ Hood/settings.py
@@ -74,14 +74,14 @@ WSGI_APPLICATION = 'Hood.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hood',
+        'USER': 'pricilla',
+    'PASSWORD':'1234',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -120,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,3 +1,65 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+import datetime as dt
 # Create your models here.
+class Neighborhood(models.Model):
+    name = models.CharField(max_length = 30)
+    user_id = models.ForeignKey(User,blank=True, on_delete=models.CASCADE,related_name='user',null=True)
+    location = models.CharField(max_length = 30)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    name = models.CharField(max_length=30)
+    email = models.EmailField(max_length = 30)
+    profile_pic = models.ImageField(upload_to='profile/')
+    pub_date_created = models.DateTimeField(auto_now_add=True, null=True)
+    neighbourhood = models.ForeignKey('Neighbourhood', blank=True, null=True)
+
+    def __str__(self):
+        return self.first_name
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profiles(cls):
+        profiles = cls.objects.all()
+        return profiles
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+    
+class Business(models.Model):
+    business_name = models.CharField(max_length = 30)
+    email = models.EmailField(max_length = 30)
+    # neighbourhood = models.ForeignKey(neighbourhood, related_name='businesses')
+
+# @classmethod
+# def search_by_name(cls,search_term):
+#     business = cls.neighbourhood = models.ForeignKey(neighbourhood, related_name='businesses')objects.filter(title__icontains=search_term)
+#     return business
+
+class Post(models.Model):
+    user = models.ForeignKey(Profile, related_name='profile')
+    post = models.CharField(max_length=30)
+    # neighbourhood = models.ForeignKey(Neighbourhood, related_name='posts')           
+
+class neighbourhood(models.Model):
+    user = models
