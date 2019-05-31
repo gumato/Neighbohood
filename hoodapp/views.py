580e00d8dc3106e69d652d45ea733ff39751e924
pricilla gumato sharamo <gumatopricilla22@gmail.com>
2019-05-31 11:20:17 +0300

    Add signing in form

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -4,4 +4,19 @@ from django.http  import HttpResponse,Http404
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    date = dt.date.today()
+    return render(request,'index.html{"date":date})
+
+def Signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+                user = form.save(commit=False)
+                user.is_active = True
+                user.save()
+                to_email = form.cleaned_data.get('email')
+                return HttpResponse('Confirm your email address to complete registration')
+           
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'Form':form})    
